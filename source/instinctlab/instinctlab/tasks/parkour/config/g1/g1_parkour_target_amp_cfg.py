75a70d40a7380eee5f2ed9dbddca06d42b0c355b
zhang <wb_z0123@163.com>
2026-07-07 11:24:42 +0800

    feat(parkour): allow motion reference path overrides

--- source/instinctlab/instinctlab/tasks/parkour/config/g1/g1_parkour_target_amp_cfg.py
+++ source/instinctlab/instinctlab/tasks/parkour/config/g1/g1_parkour_target_amp_cfg.py
@@ -31,9 +31,14 @@ G1_with_shoe_CFG.spawn.asset_path = os.path.abspath(
 
 @configclass
 class AmassMotionCfg(AmassMotionCfgBase):
-    path = os.path.expanduser("~/Datasets")
+    path = os.path.expanduser(os.environ.get("PARKOUR_MOTION_REFERENCE_DIR", "~/Datasets"))
     retargetting_func = None
-    filtered_motion_selection_filepath = os.path.expanduser("~/Datasets/parkour_motion_without_run.yaml")
+    filtered_motion_selection_filepath = os.path.expanduser(
+        os.environ.get(
+            "PARKOUR_MOTION_SELECTION_FILE",
+            "~/Datasets/parkour_motion_without_run.yaml",
+        )
+    )
     motion_start_from_middle_range = [0.0, 0.9]
     motion_start_height_offset = 0.0
     ensure_link_below_zero_ground = False
